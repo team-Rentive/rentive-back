f811150e3feda75475f7f517e8016a13b36302ba
Gihyun Kim <83570546+kimgihean@users.noreply.github.com>
2023-02-14 16:15:59 +0900

    23-02-14 김기현
    Gihyun-Test chatting, address, category 삭제

--- src/main/java/com/rent/rentservice/chatting/repository/ChattingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rent.rentservice.chatting.repository;
-
-import com.rent.rentservice.chatting.domain.Chatting;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChattingRepository extends JpaRepository<Chatting, Long> {
-}
